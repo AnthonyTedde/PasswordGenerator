f98572bdbaf3492948010378f1febaf53394827e
elpapito <antho.tedde@gmail.com>
2015-03-04 16:27:19 +0100

    add test

--- lib/password_generator.rb
+++ lib/password_generator.rb
@@ -3,8 +3,17 @@ require "password_generator/version"
 module PasswordGenerator
     # Range
     ASCII = ('!'..'~')
+    ALPHA = ASCII.grep(/[[:alpha:]]/)
     LOWER = ASCII.grep(/[[:lower:]]/)
     UPPER = ASCII.grep(/[[:upper:]]/)
     DIGIT = ASCII.grep(/[[:digit:]]/)
     PUNCT = ASCII.grep(/[[:punct:]]/)
+
+    def self.generate
+        (1..20).map do
+            ALPHA.sample
+        end.join
+    end
+
 end
+
---
+++ test/password_generator_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'password_generator'
+
+class PasswordGeneratorTest < Minitest::Test
+    def test_alpha
+        50.times do
+            assert_match /^[[:alpha:]]+$/,
+                PasswordGenerator.generate
+        end
+    end
+end
